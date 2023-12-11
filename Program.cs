b0af1778d37086d57347c091f60b326fb63dc852
Leonardo Miguel <153560379+LeoDevCoding@users.noreply.github.com>
2023-12-11 18:47:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Banda metalica = new Banda("Metalica");
+ 
+Album albumDoMetalica = new Album("Master of Puppets");
+
+Musica musica1 = new Musica(metalica, "Welcome home")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(metalica, "Disposable heroes")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
+
+albumDoMetalica.AdicionarMusica(musica1);
+albumDoMetalica.AdicionarMusica(musica2);
+metalica.AdicionarAlbum(albumDoMetalica);
+
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumDoMetalica.ExibirMusicasDoAlbum();
+metalica.ExibirDiscografia();
